122282d19ae9cbc66389e728cbe130a8f90b1413
Anamay <75950521+Anamay21@users.noreply.github.com>
2022-08-03 22:10:23 +0530

    Add files via upload

---
+++ Day-1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("\nWelcme to the Band Name Generator.")
+city_name = input("Where were you born: ")
+pet_name = input("What is your pet's name: ")
+print("Your band name could be " + city_name + " " + pet_name)
